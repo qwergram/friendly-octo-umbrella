7fd1bd70315f2d5f80116ad019f708a6a1a625e5
Norton Pengra <npengra317@gmail.com>
2016-07-21 16:07:56 -0700

    Rearrange model order

--- boardapp/models.py
+++ boardapp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Board(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    maximum_threads = models.IntegerField(default=32)
-    password = models.CharField(max_length=32)
-    threads = models.ManyToManyField(Thread, related_name="board")
-
-class Thread(models.Model):
-    head = models.ForeignKey(Post)
-    posts = models.ManyToManyField(Post, related_name="thread")
-
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     password = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
     post_id = models.IntegerField()
+
+class Thread(models.Model):
+    head = models.ForeignKey(Post)
+    posts = models.ManyToManyField(Post, related_name="thread")
+
+class Board(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    maximum_threads = models.IntegerField(default=32)
+    password = models.CharField(max_length=32)
+    threads = models.ManyToManyField(Thread, related_name="board")

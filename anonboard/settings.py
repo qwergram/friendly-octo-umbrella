4bd2b066e8696bbddac23065db252c5c582c32ca
Norton Pengra <npengra317@gmail.com>
2016-07-21 15:46:46 -0700

    Include new apps

--- anonboard/settings.py
+++ anonboard/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restframework',
+    'boardapp',
 ]
 
 MIDDLEWARE_CLASSES = [

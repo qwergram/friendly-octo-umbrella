e627aa301a0af18ea208096cd4e8b159aacfee81
Norton Pengra <npengra317@gmail.com>
2016-07-21 16:01:10 -0700

    Register models to admin page

--- boardapp/admin.py
+++ boardapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from anonboard import models
 
 # Register your models here.
+
+admin.site.register(models.Board)
+admin.site.register(models.Thread)
+admin.site.register(models.Post)

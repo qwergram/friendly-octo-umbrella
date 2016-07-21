1f5918dd9e5c1394b9e7be089e2db02618dd8ab1
Norton Pengra <npengra317@gmail.com>
2016-07-21 16:16:07 -0700

    Allow blanks in some fields

--- boardapp/models.py
+++ boardapp/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
     content = models.TextField()
-    password = models.CharField(max_length=32)
+    password = models.CharField(max_length=32, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     post_id = models.IntegerField()
 
 class Thread(models.Model):
     head = models.ForeignKey(Post, related_name="head_of")
-    posts = models.ManyToManyField(Post, related_name="thread")
+    posts = models.ManyToManyField(Post, related_name="thread", null=True, blank=True)
 
 class Board(models.Model):
     name = models.CharField(max_length=255, unique=True)
     maximum_threads = models.IntegerField(default=32)
     password = models.CharField(max_length=32)
-    threads = models.ManyToManyField(Thread, related_name="board")
+    threads = models.ManyToManyField(Thread, related_name="board", null=True, blank=True)

63315fac39e5d11bde9d0f2378a927d29063145f
Norton Pengra <npengra317@gmail.com>
2016-08-05 16:12:06 -0700

    Defined __str__ attributes

--- boardapp/models.py
+++ boardapp/models.py
@@ -9,12 +9,24 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     post_id = models.IntegerField()
 
+    def __str__(self):
+        return self.title
+
 class Thread(models.Model):
     head = models.ForeignKey(Post, related_name="head_of")
     posts = models.ManyToManyField(Post, related_name="thread", null=True, blank=True)
+
+    def __str__(self):
+        try:
+            return self.head.title
+        except AttributeError:
+            return "[Untitled]"
 
 class Board(models.Model):
     name = models.CharField(max_length=255, unique=True)
     maximum_threads = models.IntegerField(default=32)
     password = models.CharField(max_length=32)
     threads = models.ManyToManyField(Thread, related_name="board", null=True, blank=True)
+
+    def __str__(self):
+        return self.name

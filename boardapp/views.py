7d3499bef41ce89851bc2c0ae2be76f3da26855a
Norton Pengra <npengra317@gmail.com>
2016-09-20 13:16:20 -0700

    Create Thread Index view

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views import generic
 from boardapp.models import Board
 
@@ -10,4 +10,14 @@ class BoardIndex(generic.TemplateView):
     def get_context_data(self):
         context = super().get_context_data()
         context['boards'] = Board.objects.all()
+        return context
+
+class ThreadIndex(generic.TemplateView):
+    template_name = "boardapp/list_threads.html"
+
+    def get_context_data(self, boardname):
+        context = super().get_context_data()
+        board = get_object_or_404(Board, shortcut=boardname)
+        context['board'] = board
+        # import pdb; pdb.set_trace()
         return context

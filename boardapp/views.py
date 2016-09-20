3059fbc1273ce77ebe456a64a84654d4abccbcc9
Norton Pengra <npengra317@gmail.com>
2016-09-20 08:20:03 -0700

    show board objects

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponse
 from django.views import generic
+from boardapp.models import Board
 
 # Create your views here.
 
 class BoardIndex(generic.View):
 
     def get(self, request):
-        return HttpResponse("ok!")
+        boards = Board.objects.all()
+        return HttpResponse(str([str(x) for x in boards]))

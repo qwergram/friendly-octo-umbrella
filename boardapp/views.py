2b99c2dd0cc960c8a098dcbe3b2fe5452feab303
Norton Pengra <npengra317@gmail.com>
2016-09-21 09:53:32 -0700

    view for new route

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.views import generic
-from boardapp.models import Board
+from boardapp.models import Board, Thread
 
 # Create your views here.
 
@@ -20,4 +20,14 @@ class ThreadIndex(generic.TemplateView):
         board = get_object_or_404(Board, shortcut=boardname)
         context['board'] = board
         # import pdb; pdb.set_trace()
+        return context
+
+
+class ThreadView(generic.TemplateView):
+    template_name = "boardapp/view_thread.html"
+
+    def get_context_data(self, boardname, threadid):
+        context = super().get_context_data()
+        thread = get_object_or_404(Thread, pk=threadid)
+        context['thread'] = thread
         return context
